f0de1a36e4ce7bed98b12e94524658aacaee81c8
Dugal Harris <dugalh@gmail.com>
2022-04-05 10:46:48 +0200

    add rasterio & tqdm dependencies and tidy

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
     url="https://github.com/dugalh/geedim",
     license="Apache-2.0",
     packages=find_packages(exclude=['tests', 'data'], include=['geedim']),
-    install_requires=["pandas>=1.1, <2", "earthengine-api>=0.1.2, <1", "click>=8, <9", "requests>=2.2, <3",
-                      "numpy>=1"],
+    install_requires=["numpy>=1.19", "rasterio>=1.1", "click>=8", "tqdm>=4.6", "pandas>=1.1", "earthengine-api>=0.1.2",
+                      "requests>=2.2"],
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
