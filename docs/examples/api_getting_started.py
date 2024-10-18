11f90ff1b9dda48afd1f441fa2bd2374b76ab48f
Dugal Harris <dugalh@gmail.com>
2024-10-18 11:08:25 +0200

    update s2 e.g. to use harmonised image with cloud-score

--- docs/examples/api_getting_started.py
+++ docs/examples/api_getting_started.py
@@ -1,14 +1,14 @@
 # [initialise-start]
 import geedim as gd
+
 gd.Initialize()
 # [initialise-end]
 
 # [search-start]
 # geojson search polygon
 region = {
-    'type': 'Polygon', 'coordinates': [[
-        (19, -34), (19, -33.8), (18.8, -33.8), (18.8, -34), (19., -34)
-    ]]
+    'type': 'Polygon',
+    'coordinates': [[(19, -34), (19, -33.8), (18.8, -33.8), (18.8, -34), (19.0, -34)]],
 }
 
 # create & search a landsat-8 collection, reporting cloudless portions
@@ -22,13 +22,11 @@ print(filt_coll.properties_table)
 
 # [image-download-start]
 # create a landsat-8 image from its ID
-im = gd.MaskedImage.from_id(
-    'LANDSAT/LC08/C02/T1_L2/LC08_175083_20190117', mask=False)
+im = gd.MaskedImage.from_id('LANDSAT/LC08/C02/T1_L2/LC08_175083_20190117', mask=False)
 
 # download a region of the image with 'average' resampling to 60m pixels, and
 # data type conversion to 16 bit unsigned int
-im.download('landsat_8_image.tif', region=region, resampling='average',
-            scale=60, dtype='uint16')
+im.download('landsat_8_image.tif', region=region, resampling='average', scale=60, dtype='uint16')
 # [image-download-end]
 
 # [composite-start]
@@ -36,21 +34,22 @@ im.download('landsat_8_image.tif', region=region, resampling='average',
 # the least cloudy image by specifying `region`
 comp_im = filt_coll.composite(method='q-mosaic', region=region)
 # download the composite, specifying crs, region, and scale
-comp_im.download('landsat_8_comp_image.tif', region=region, crs='EPSG:32634',
-                 scale=30)
+comp_im.download('landsat_8_comp_image.tif', region=region, crs='EPSG:32634', scale=30)
 # [composite-end]
 
 # [mask-start]
 # create a cloud/shadow masked Sentinel-2 image, specifying a cloud
-# probability threshold of 30%
+# score threshold of 0.7
 im = gd.MaskedImage.from_id(
-    'COPERNICUS/S2_SR/20190101T082331_20190101T084846_T34HCH', mask=True, prob=30)
+    'COPERNICUS/S2_SR_HARMONIZED/20190101T082331_20190101T084846_T34HCH', mask=True, score=0.7
+)
 # download a region of the masked image, downsampling to 20m pixels
-im.download('s2_sr_image.tif', region=region, scale=20, resampling='average')
+im.download('s2_sr_hm_image.tif', region=region, scale=20, resampling='average')
 # [mask-end]
 
 # [metadata-start]
 import rasterio as rio
+
 with rio.open('s2_sr_image.tif', 'r') as ds:
     print('Image properties:\n', ds.tags())
     print('Band names:\n', ds.descriptions)
@@ -59,8 +58,9 @@ with rio.open('s2_sr_image.tif', 'r') as ds:
 
 # [max_tile_size-start]
 import ee
+
 # create a computed ee.Image
-ee_im = ee.Image('COPERNICUS/S2_SR/20190101T082331_20190101T084846_T34HCH')
+ee_im = ee.Image('COPERNICUS/S2_SR_HARMONIZED/20190101T082331_20190101T084846_T34HCH')
 comp_im = ee_im.select('B3').entropy(ee.Kernel.square(5))
 
 # encapsulate in MaskedImage, and download with max_tile_size=8
