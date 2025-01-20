c5098c8d292cb588e2c8d192ad9442c426e2b543
Dugal Harris <dugalh@gmail.com>
2025-01-20 19:40:32 +0200

    relax search param checks

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -1,17 +1,17 @@
 """
-    Copyright 2021 Dugal Harris - dugalh@gmail.com
+Copyright 2021 Dugal Harris - dugalh@gmail.com
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from __future__ import annotations
@@ -365,11 +365,6 @@ def test_search_no_fill_or_cloudless_portion(
 def test_search_errors():
     """Test MaskedCollection.search() argument combination errors."""
     gd_coll = MaskedCollection.from_name('COPERNICUS/S2_HARMONIZED')
-
-    # no start_date or region args
-    with pytest.raises(ValueError) as ex:
-        gd_coll.search()
-    assert 'region' in str(ex.value) and 'start_date' in str(ex.value)
 
     # fill_portion/cloudless_portion but no region arg
     with pytest.raises(ValueError) as ex:
