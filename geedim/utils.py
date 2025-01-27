10c9e17ffdee972d02abd46f6dbcee01805e8bbf
Dugal Harris <dugalh@gmail.com>
2025-01-27 19:35:56 +0200

    remove unused functions, change to RST docstrings & fix lint warnings

--- geedim/utils.py
+++ geedim/utils.py
@@ -26,10 +26,10 @@ import pathlib
 import sys
 import time
 import warnings
+from collections.abc import Coroutine
 from concurrent.futures import ThreadPoolExecutor
-from contextlib import contextmanager
 from threading import Thread
-from typing import Any, Callable, Coroutine, Generic, Optional, Tuple, TypeVar
+from typing import Any, Callable, Generic, TypeVar
 
 if sys.version_info >= (3, 11):
     from asyncio.runners import Runner
@@ -39,17 +39,10 @@ else:
 
 import aiohttp
 import ee
-import numpy as np
 import rasterio as rio
-import requests
 from rasterio import warp
 from rasterio.env import GDALVersion
-from rasterio.windows import Window
-from requests.adapters import HTTPAdapter, Retry
 from tqdm.auto import tqdm
-
-from geedim.enums import ResamplingMethod
-from geedim.errors import GeedimError
 
 logger = logging.getLogger(__name__)
 
@@ -62,28 +55,23 @@ _GDAL_AT_LEAST_35 = GDALVersion.runtime().at_least("3.5")
 T = TypeVar('T')
 
 
-def Initialize(opt_url: Optional[str] = 'https://earthengine-highvolume.googleapis.com', **kwargs):
+def Initialize(opt_url: str = 'https://earthengine-highvolume.googleapis.com', **kwargs) -> None:
     """
     Initialise Earth Engine.
 
-    Credentials will be read from the `EE_SERVICE_ACC_PRIVATE_KEY` environment variable if it exists
-    (useful for integrating with e.g. GitHub actions).
+    Credentials will be read from the `EE_SERVICE_ACC_PRIVATE_KEY` environment variable if it
+    exists (useful for integrating with e.g. GitHub actions).
 
-    .. note::
-
-        Earth Engine recommends using the `high volume endpoint` for applications like ``geedim``.
-        See `the docs <https://developers.google.com/earth-engine/cloud/highvolume>`_ for more information.
-
-    Parameters
-    ----------
-    opt_url: str
-        The Earth Engine endpoint to use.  If ``None``, the default is used.
-    kwargs
-        Optional arguments to pass to `ee.Initialize`.
+    :param opt_url:
+        The Earth Engine endpoint to use.  Defaults to the high volume endpoint.  See `the Earth
+        Engine docs <https://developers.google.com/earth-engine/guides/processing_environments
+        #endpoints>`_ for more information.
+    :param kwargs:
+        Optional arguments to pass to ``ee.Initialize``.
     """
-
+    # TODO: is the high vol endpoint still the right default value here?
     if not ee.data._credentials:
-        # Adpated from https://gis.stackexchange.com/questions/380664/how-to-de-authenticate-from-earth-engine-api.
+        # Adapted from https://gis.stackexchange.com/questions/380664/how-to-de-authenticate-from-earth-engine-api.
         env_key = 'EE_SERVICE_ACC_PRIVATE_KEY'
 
         if env_key in os.environ:
@@ -111,118 +99,63 @@ def singleton(cls: T, *args, **kwargs) -> T:
 
 def split_id(image_id: str) -> tuple[str | None, str | None]:
     """
-    Split Earth Engine image ID into collection and index components.
+    Split an Earth Engine image ID into collection and index components.
 
-    Parameters
-    ----------
-    image_id: str
-        Earth engine image ID.
+    :param image_id:
+        Earth Engine ID.
 
-    Returns
-    -------
-    tuple(str, str)
-        A tuple of strings: (collection name, image index).
+    :return:
+        A tuple of the image collection name and image index.
     """
     if not image_id:
         return None, None
-    index = image_id.split("/")[-1]
-    ee_coll_name = "/".join(image_id.split("/")[:-1])
+    parts = image_id.strip('/').split('/')
+    index = parts[-1]
+    ee_coll_name = '/'.join(parts[:-1])
     return ee_coll_name, index
 
 
-@contextmanager
-def suppress_rio_logs(level: int = logging.ERROR):
-    """A context manager that sets the `rasterio` logging level, then returns it to its original value."""
-    # TODO: this should not be necessary if logging level changes are limited to geedim.  if it has to be used,
-    #  it should be made thread-safe.
-    try:
-        # GEE sets GeoTIFF `colorinterp` tags incorrectly. This suppresses `rasterio` warning relating to this:
-        # 'Sum of Photometric type-related color channels and ExtraSamples doesn't match SamplesPerPixel'
-        rio_level = logging.getLogger('rasterio').getEffectiveLevel()
-        logging.getLogger('rasterio').setLevel(level)
-        yield
-    finally:
-        logging.getLogger('rasterio').setLevel(rio_level)
-
-
-def get_bounds(filename: pathlib.Path, expand: float = 5):
+def get_bounds(filename: pathlib.Path, expand: float = 5) -> dict:
     """
-    Get a geojson polygon representing the bounds of an image.
+    Return a GeoJSON geometry representing the bounds of an image file.
 
-    Parameters
-    ----------
-    filename: str, pathlib.Path
-        Path of the image file whose bounds to find.
-    expand : int, optional
-        Percentage (0-100) by which to expand the bounds (default: 5).
+    :param filename:
+        Path to the image file.
+    :param expand:
+        Percentage (0-100) by which to expand the bounds.
 
-    Returns
-    -------
-    dict
-        Geojson polygon.
+    :return:
+        GeoJSON geometry.
     """
-    with suppress_rio_logs(), rio.Env(GTIFF_FORCE_RGBA=False), rio.open(filename) as im:
-        bbox = im.bounds
-        if (im.crs.linear_units == "metre") and (expand > 0):  # expand the bounding box
-            expand_x = (bbox.right - bbox.left) * expand / 100.0
-            expand_y = (bbox.top - bbox.bottom) * expand / 100.0
-            bbox_expand = rio.coords.BoundingBox(
-                bbox.left - expand_x,
-                bbox.bottom - expand_y,
-                bbox.right + expand_x,
-                bbox.top + expand_y,
+    with rio.Env(GTIFF_FORCE_RGBA=False), rio.open(filename) as im:
+        bounds = im.bounds
+        if expand != 0:
+            expand_x = (bounds.right - bounds.left) * expand / 100.0
+            expand_y = (bounds.top - bounds.bottom) * expand / 100.0
+            bounds = rio.coords.BoundingBox(
+                bounds.left - expand_x,
+                bounds.bottom - expand_y,
+                bounds.right + expand_x,
+                bounds.top + expand_y,
             )
-        else:
-            bbox_expand = bbox
 
-        coordinates = [
-            [bbox_expand.right, bbox_expand.bottom],
-            [bbox_expand.right, bbox_expand.top],
-            [bbox_expand.left, bbox_expand.top],
-            [bbox_expand.left, bbox_expand.bottom],
-            [bbox_expand.right, bbox_expand.bottom],
+        coords = [
+            [bounds.right, bounds.bottom],
+            [bounds.right, bounds.top],
+            [bounds.left, bounds.top],
+            [bounds.left, bounds.bottom],
+            [bounds.right, bounds.bottom],
         ]
 
-        bbox_expand_dict = dict(type="Polygon", coordinates=[coordinates])
-        src_bbox_wgs84 = warp.transform_geom(
-            im.crs, "WGS84", bbox_expand_dict
-        )  # convert to WGS84 geojson
-    return src_bbox_wgs84
-
-
-def get_projection(image: ee.Image, min_scale: bool = True) -> ee.Projection:
-    """
-    Get the min/max scale projection of image bands.  Server side - no calls to getInfo().
-    Adapted from from https://github.com/gee-community/gee_tools, MIT license.
-
-    Parameters
-    ----------
-    image : ee.Image
-            Image whose min/max projection to retrieve.
-    min_scale: bool, optional
-         Retrieve the projection corresponding to the band with the minimum (True) or maximum (False) scale.
-         (default: True)
-
-    Returns
-    -------
-    ee.Projection
-        Requested projection.
-    """
-    if not isinstance(image, ee.Image):
-        raise TypeError('image is not an instance of ee.Image')
-
-    bands = image.bandNames()
-    scales = bands.map(lambda band: image.select(ee.String(band)).projection().nominalScale())
-    projs = bands.map(lambda band: image.select(ee.String(band)).projection())
-    projs = projs.sort(scales)
-
-    return ee.Projection(projs.get(0) if min_scale else projs.get(-1))
+        poly = dict(type='Polygon', coordinates=[coords])
+        poly = warp.transform_geom(im.crs, 'EPSG:4326', poly)
+    return poly
 
 
 class Spinner(Thread):
     def __init__(self, label='', interval=0.2, leave=True, **kwargs):
         """
-        Thread sub-class to run a non-blocking spinner.
+        Thread subclass to run a non-blocking spinner.
 
         :param label:
             Prepend spinner with this label.
@@ -253,7 +186,7 @@ class Spinner(Thread):
 
     def run(self):
         """Run the spinner thread."""
-        cursors_it = itertools.cycle('/-\|')
+        cursors_it = itertools.cycle(r'/-\|')
 
         while self._run:
             cursor = next(cursors_it)
@@ -279,104 +212,11 @@ class Spinner(Thread):
         self._run = False
 
 
-def resample(ee_image: ee.Image, method: ResamplingMethod) -> ee.Image:
-    """
-    Resample an ee.Image. Extends ee.Image.resample by only resampling when the image has a fixed projection, and by
-    providing an additional 'average' method for downsampling.  This logic is performed server-side.
-
-    Note that for the :attr:`ResamplingMethod.average` ``method``, the returned image has a minimum scale default
-    projection.
-
-    See https://developers.google.com/earth-engine/guides/resample for more info.
-
-    Parameters
-    ----------
-    ee_image: ee.Image
-        Image to resample.
-    method: ResamplingMethod
-        Resampling method to use.
-
-    Returns
-    -------
-    ee_image: ee.Image
-        Resampled image.
-    """
-    # TODO : use STAC to only resample continuous qty type bands
-    method = ResamplingMethod(method)
-    if method == ResamplingMethod.near:
-        return ee_image
-
-    # resample the image, if it has a fixed projection
-    proj = get_projection(ee_image, min_scale=True)
-    has_fixed_proj = (
-        proj.crs().compareTo('EPSG:4326').neq(0).Or(proj.nominalScale().toInt64().neq(111319))
-    )
-
-    def _resample(ee_image: ee.Image) -> ee.Image:
-        """Resample the given image, allowing for additional 'average' method."""
-        if method == ResamplingMethod.average:
-            # set the default projection to the minimum scale projection (required for e.g. S2 images that have
-            # non-fixed projection bands)
-            ee_image = ee_image.setDefaultProjection(proj)
-            return ee_image.reduceResolution(reducer=ee.Reducer.mean(), maxPixels=1024)
-        else:
-            return ee_image.resample(method.value)
-
-    return ee.Image(ee.Algorithms.If(has_fixed_proj, _resample(ee_image), ee_image))
-
-
-def retry_session(
-    retries: int = 5,
-    backoff_factor: float = 2.0,
-    status_forcelist: Tuple = (429, 500, 502, 503, 504),
-    session: requests.Session = None,
-    **kwargs,
-) -> requests.Session:
-    """requests session configured for retries."""
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = HTTPAdapter(max_retries=retry, **kwargs)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
-
-
-def expand_window_to_grid(win: Window, expand_pixels: Tuple[int, int] = (0, 0)) -> Window:
-    """
-    Expand rasterio window extents to the nearest whole numbers i.e. for ``expand_pixels`` >= (0, 0), it will return a
-    window that contains the original extents.
-
-    Parameters
-    ----------
-    win: rasterio.windows.Window
-        Window to expand.
-    expand_pixels: tuple, optional
-        Tuple specifying the number of (rows, columns) pixels to expand the window by.
-
-    Returns
-    -------
-    rasterio.windows.Window
-        Expanded window.
-    """
-    col_off, col_frac = np.divmod(win.col_off - expand_pixels[1], 1)
-    row_off, row_frac = np.divmod(win.row_off - expand_pixels[0], 1)
-    width = np.ceil(win.width + 2 * expand_pixels[1] + col_frac)
-    height = np.ceil(win.height + 2 * expand_pixels[0] + row_frac)
-    exp_win = Window(int(col_off), int(row_off), int(width), int(height))
-    return exp_win
-
-
 def rio_crs(crs: str | rio.CRS) -> str | rio.CRS:
     """Convert a GEE CRS string to a rasterio compatible CRS string."""
     if crs == 'SR-ORG:6974':
-        # This is a workaround for https://issuetracker.google.com/issues/194561313, that replaces the alleged GEE
-        # SR-ORG:6974 with actual WKT for SR-ORG:6842 taken from
+        # This is a workaround for https://issuetracker.google.com/issues/194561313,
+        # that replaces the alleged GEE SR-ORG:6974 with actual WKT for SR-ORG:6842 taken from
         # https://github.com/OSGeo/spatialreference.org/blob/master/scripts/sr-org.json.
         crs = """PROJCS["Sinusoidal",
         GEOGCS["GCS_Undefined",
@@ -392,23 +232,20 @@ def rio_crs(crs: str | rio.CRS) -> str | rio.CRS:
     return crs
 
 
-def asset_id(image_id: str, folder: str = None):
+def asset_id(filename: str, folder: str | None = None):
     """
-    Convert an EE image ID and EE asset project into an EE asset ID.
+    Convert a ``filename`` and ``folder`` into an Earth Engine asset ID.
 
-    If ``folder`` is not supplied, ``image_id`` is returned as is. Otherwise, ``image_id`` is
-    converted to a name by changing forward slashes to dashes, ``folder`` is split into <root
-    folder> and <sub folder> sections, and a string is returned with EE asset ID format:
+    If ``folder`` is not supplied, ``filename`` is returned as is.  Otherwise ``folder`` is
+    split to create an Earth Engine asset ID as:
 
-        projects/<root folder>/assets/<sub folder(s)>/<image_name>.
+        projects/<root folder>/assets/<sub folder(s)>/<filename>.
     """
     if not folder:
-        return image_id
-    im_name = image_id.replace('/', '-')
-    folder = pathlib.PurePosixPath(folder)
-    cloud_folder = pathlib.PurePosixPath(folder.parts[0])
-    asset_path = pathlib.PurePosixPath('/'.join(folder.parts[1:])).joinpath(im_name)
-    return f'projects/{str(cloud_folder)}/assets/{str(asset_path)}'
+        return filename
+    parts = folder.strip('/').split('/')
+    asset_path = '/'.join(parts[1:] + [filename])
+    return f'projects/{parts[0]}/assets/{asset_path}'
 
 
 def register_accessor(name: str, cls: type) -> Callable[[type[T]], type[T]]:
@@ -416,7 +253,7 @@ def register_accessor(name: str, cls: type) -> Callable[[type[T]], type[T]]:
     Decorator function to register a custom accessor on a given class.
 
     Adapted from https://github.com/pydata/xarray/blob/main/xarray/core/extensions.py, Apache-2.0
-    License.
+    Licence.
 
     :param name:
         Name under which the accessor should be registered.
@@ -454,7 +291,7 @@ def register_accessor(name: str, cls: type) -> Callable[[type[T]], type[T]]:
             except AttributeError as ex:
                 # __getattr__ on data object will swallow any AttributeErrors raised when
                 # initializing the accessor, so we need to raise as something else
-                raise GeedimError(f"Error initializing {self._name!r} accessor.") from ex
+                raise RuntimeError(f"Error initializing {self._name!r} accessor.") from ex
 
             cache[self._name] = accessor_obj
             return accessor_obj
@@ -465,6 +302,7 @@ def register_accessor(name: str, cls: type) -> Callable[[type[T]], type[T]]:
                 f"Registration of accessor {accessor!r} under name '{name}' for type {cls!r} is"
                 "overriding a preexisting attribute with the same name.",
                 category=RuntimeWarning,
+                stacklevel=2,
             )
         setattr(cls, name, CachedAccessor(name, accessor))
         return accessor
@@ -472,7 +310,7 @@ def register_accessor(name: str, cls: type) -> Callable[[type[T]], type[T]]:
     return decorator
 
 
-def get_tqdm_kwargs(desc: str = None, unit: str = None, **kwargs) -> dict[str, Any]:
+def get_tqdm_kwargs(desc: str | None = None, unit: str | None = None, **kwargs) -> dict[str, Any]:
     """Return a dictionary of kwargs for a tqdm progress bar."""
     tqdm_kwargs: dict[str, Any] = dict(dynamic_ncols=True, leave=True)
     tqdm_kwargs.update(**kwargs)
@@ -499,8 +337,8 @@ class AsyncRunner:
         A singleton that manages the lifecycle of an :mod:`~python.asyncio` event loop and
         :mod:`aiohttp` client session.
 
-        The :meth:`close` method is executed on normal python exit.  Can also be used as a
-        context manager.
+        The :meth:`close` method is executed on normal python exit.  This class can also be used
+        as a context manager.
 
         :param kwargs:
             Optional keywords arguments to :class:`~python.asyncio.runners.Runner`.
@@ -521,7 +359,9 @@ class AsyncRunner:
     def __del__(self):
         if not self._closed:
             warnings.warn(
-                f'{type(self).__name__} was never closed: {self!r}.', category=ResourceWarning
+                f'{type(self).__name__} was never closed: {self!r}.',
+                category=ResourceWarning,
+                stacklevel=2,
             )
 
     def close(self):
