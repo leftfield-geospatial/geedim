79ca5a453930f4a57216dbfc528bff33a9589ed6
Dugal Harris <dugalh@gmail.com>
2025-02-03 11:49:37 +0200

    fix typing

--- geedim/medoid.py
+++ geedim/medoid.py
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from __future__ import annotations
+
 import ee
 
 from geedim.enums import SpectralDistanceMetric
