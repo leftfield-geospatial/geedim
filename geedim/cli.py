750d1b28ec996991df74b63475dcd674632dac4c
Dugal Harris <dugalh@gmail.com>
2025-05-14 21:43:30 +0200

    refactor for collection export:
    - only Initialize() EE on subcommands w/o --help
    - add --buffer and --split options
    - export from collection not image
    - fix flag options for https://github.com/pallets/click/issues/2894

--- geedim/cli.py
+++ geedim/cli.py
@@ -34,20 +34,20 @@ from fsspec.core import OpenFile
 from rasterio.warp import transform_bounds
 from tqdm.auto import tqdm
 
-from geedim import schema, utils, version
-from geedim.enums import (
-    CloudMaskMethod,
-    CloudScoreBand,
-    CompositeMethod,
-    Driver,
-    ExportType,
-    ResamplingMethod,
-)
+from geedim import enums, schema, utils, version
 from geedim.image import ImageAccessor, _nodata_vals
-from geedim.mask import _get_class_for_id
 from geedim.tile import Tiler
 
 logger = logging.getLogger(__name__)
+
+
+class _EECommand(click.Command):
+    """click Command sub-class for initialising Earth Engine."""
+
+    def invoke(self, ctx: click.Context) -> Any:
+        # initialise here rather than in cli() so that --help is not delayed
+        utils.Initialize()
+        return super().invoke(ctx)
 
 
 def _collection_cb(ctx: click.Context, param: click.Parameter, name: str) -> str:
@@ -76,19 +76,19 @@ def _crs_cb(ctx: click.Context, param: click.Parameter, crs: str) -> str | None:
 
 def _bbox_cb(
     ctx: click.Context, param: click.Parameter, bounds: Sequence[float]
-) -> ee.Geometry | None:
+) -> dict[str, Any] | None:
     """click callback to convert --bbox to a GeoJSON polygon."""
     if bounds == '-':
         if 'region' not in ctx.obj:
             raise click.BadParameter("No piped bounds or region available.")
         bounds = ctx.obj['region']
     elif bounds:
-        bounds = ee.Geometry.Rectangle(*bounds)
+        bounds = _bounds_to_geojson(*bounds)
         ctx.obj['region'] = bounds
     return bounds
 
 
-def _region_cb(ctx: click.Context, param: click.Parameter, region: str) -> ee.Geometry | None:
+def _region_cb(ctx: click.Context, param: click.Parameter, region: str) -> dict[str, Any] | None:
     """click callback to read --region and convert to an GeoJSON polygon."""
     if region == '-':
         if 'region' not in ctx.obj:
@@ -98,11 +98,11 @@ def _region_cb(ctx: click.Context, param: click.Parameter, region: str) -> ee.Ge
         try:
             if region.lower().endswith('json'):
                 with fsspec.open(region, 'rt', encoding='utf-8') as f:
-                    region = ee.Geometry(json.load(f))
+                    region = json.load(f)
             else:
                 with rio.open(fsspec.open(region, 'rb'), 'r') as ds:
                     bounds = transform_bounds(ds.crs, 'EPSG:4326', *ds.bounds)
-                    region = ee.Geometry.Rectangle(*bounds)
+                    region = _bounds_to_geojson(*bounds)
         except Exception as ex:
             raise click.BadParameter(str(ex)) from ex
         ctx.obj['region'] = region
@@ -135,6 +135,12 @@ def _like_cb(ctx: click.Context, param: click.Parameter, uri_path: str) -> dict[
     return None
 
 
+def _bounds_to_geojson(left: float, bottom: float, right: float, top: float) -> dict[str, Any]:
+    """Convert bounds to a GeoJSON polygon."""
+    coords = [[left, top], [left, bottom], [right, bottom], [right, top]]
+    return dict(type="Polygon", coordinates=[coords], evenOdd=True)
+
+
 def _configure_logging(verbosity: int):
     """Configure logging level, redirecting warnings to the logger and logs to ``tqdm.write()``."""
     # configure the package logger (adapted from rasterio:
@@ -155,15 +161,8 @@ def _configure_logging(verbosity: int):
     warnings.showwarning = showwarning
 
 
-def _get_im_name(im: ee.Image) -> str:
-    """Return a name for the given image."""
-    return im.gd.id.replace('/', '-')
-
-
 def _get_images(obj: dict[str, Any], image_ids: tuple[str]) -> list[ee.Image]:
-    """Return a list of images in the click context object combined with images created from the EE
-    IDs.
-    """
+    """Combine piped images in the click context object with images created from --id image IDs."""
     images = [ee.Image(ee_id) for ee_id in image_ids] + obj.get('images', [])
     if len(images) == 0:
         raise click.UsageError(
@@ -172,17 +171,17 @@ def _get_images(obj: dict[str, Any], image_ids: tuple[str]) -> list[ee.Image]:
     return images
 
 
-def _prepare_images_for_export(
+def _prepare_export_collection(
     images: list[ee.Image],
     cloud_kwargs: dict | None = None,
-    bbox: ee.Geometry | None = None,
-    region: ee.Geometry | None = None,
+    bbox: dict[str, Any] | None = None,
+    region: dict[str, Any] | None = None,
+    buffer: float | None = None,
     like: dict[str, Any] | None = None,
     mask: bool = False,
-    buffer: float | None = None,
     **kwargs,
-) -> list[ee.Image]:
-    """Prepare images for export."""
+) -> ee.ImageCollection:
+    """Create an image collection and prepare it for export."""
     # resolve and buffer region
     region = bbox or region
     if buffer is not None:
@@ -190,39 +189,18 @@ def _prepare_images_for_export(
             raise click.UsageError(
                 "'-b' / '--bbox' or '-r' / '--region' is required with '-buf' / '--buffer'"
             )
-        region = region.buffer(buffer)
+        region = ee.Geometry(region).buffer(buffer)
 
     if like:
         # overwrite crs, crs_transform and shape from --like
         kwargs.update(**like)
 
-    # cloud/shadow masking (uses one getInfo() call to get all image IDs, rather than requiring
-    # two calls per image if the 'gd' accessor was used)
-    # TODO: avoid getInfo() here and in ImageCollectionAccessor.fromImages()?
-    cs_images = []
-    im_ids = ee.List(images).map(lambda im: ee.Image(im).get('system:id')).getInfo()
-    for im, im_id in zip(images, im_ids):
-        cls = _get_class_for_id(im_id)
-        im = cls.add_mask_bands(im, **(cloud_kwargs or {}))
-        if mask:
-            im = cls.mask_clouds(im)
-        cs_images.append(im)
-
-    # apply export params (with one getInfo() to cache all image infos)
-    cs_infos = ee.List(cs_images).getInfo()
-    prepared_images = [
-        ImageAccessor._with_info(im, info).prepareForExport(region=region, **kwargs)
-        for im, info in zip(cs_images, cs_infos)
-    ]
-
-    # cache accessors & infos (again... with one getInfo())
-    cached_images = []
-    prepared_infos = ee.List(prepared_images).getInfo()
-    for im, info in zip(prepared_images, prepared_infos):
-        im.gd = ImageAccessor._with_info(im, info)
-        cached_images.append(im)
-
-    return cached_images
+    coll = ee.ImageCollection.gd.fromImages(images)
+    coll = coll.gd.addMaskBands(**(cloud_kwargs or {}))
+    if mask:
+        coll = coll.gd.maskClouds()
+    coll = coll.gd.prepareForExport(region=region, **kwargs)
+    return coll
 
 
 # Define click options that are common to more than one command
@@ -293,7 +271,7 @@ mask_option = click.option(
 resampling_option = click.option(
     '-rs',
     '--resampling',
-    type=click.Choice(ResamplingMethod, case_sensitive=True),
+    type=click.Choice(enums.ResamplingMethod, case_sensitive=True),
     default=ImageAccessor._default_resampling,
     show_default=True,
     help='Resampling method to use when reprojecting.',
@@ -350,6 +328,15 @@ band_name_option = click.option(
     show_default='all bands',
     help='Band name(s) to export.',
 )
+split_option = click.option(
+    '-sp',
+    '--split',
+    type=click.Choice(enums.SplitType, case_sensitive=True),
+    default=enums.SplitType.images,
+    show_default=True,
+    help="Whether to export an image file for each input image, or for each band of the input "
+    "image(s).",
+)
 
 
 # geedim CLI and chained command group
@@ -362,7 +349,6 @@ def cli(ctx: click.Context, verbose: int, quiet: int):
     """Search, composite and export Google Earth Engine imagery."""
     ctx.obj = {}
     _configure_logging(verbose - quiet)
-    utils.Initialize()
     # prevent GDAL from searching for sidecar files (speeds up reads of remote files)
     ctx.with_resource(rio.Env(GDAL_DISABLE_READDIR_ON_OPEN='EMPTY_DIR'))
 
@@ -388,8 +374,8 @@ def cli(ctx: click.Context, verbose: int, quiet: int):
 @click.option(
     '-mm',
     '--mask-method',
-    type=click.Choice(CloudMaskMethod, case_sensitive=True),
-    default=CloudMaskMethod.cloud_score,
+    type=click.Choice(enums.CloudMaskMethod, case_sensitive=True),
+    default=enums.CloudMaskMethod.cloud_score,
     show_default=True,
     help='Method used to mask clouds.  Valid for Sentinel-2 images.',
 )
@@ -460,8 +446,8 @@ def cli(ctx: click.Context, verbose: int, quiet: int):
 @click.option(
     '-cb',
     '--cs-band',
-    type=click.Choice(CloudScoreBand, case_sensitive=True),
-    default=CloudScoreBand.cs,
+    type=click.Choice(enums.CloudScoreBand, case_sensitive=True),
+    default=enums.CloudScoreBand.cs,
     show_default=True,
     help="Cloud Score+ band to threshold. Valid for Sentinel-2 images with the 'cloud-score' "
     '--mask-method.',
@@ -485,7 +471,7 @@ def config(ctx: click.Context, **kwargs):
 
 
 # search command
-@cli.command(epilog='See https://geedim.readthedocs.io/ for more details.')
+@cli.command(cls=_EECommand, epilog='See https://geedim.readthedocs.io/ for more details.')
 @click.option(
     '-c',
     '--collection',
@@ -569,8 +555,8 @@ def config(ctx: click.Context, **kwargs):
 def search(
     ctx: click.Context,
     coll_id: str,
-    bbox: ee.Geometry | None,
-    region: ee.Geometry | None,
+    bbox: dict[str, Any] | None,
+    region: dict[str, Any] | None,
     output: str | None,
     add_props: tuple[str] | None,
     **kwargs,
@@ -623,11 +609,12 @@ def search(
 
 
 # download command
-@cli.command(epilog='See https://geedim.readthedocs.io/ for more details.')
+@cli.command(cls=_EECommand, epilog='See https://geedim.readthedocs.io/ for more details.')
 @image_ids_option
 @crs_option
 @bbox_option
 @region_option
+@buffer_option
 @scale_option
 @crs_transform_option
 @shape_option
@@ -637,17 +624,7 @@ def search(
 @mask_option
 @resampling_option
 @scale_offset_option
-@buffer_option
-# TODO: reorder options (e.g. --download-dir with --overwrite ?)
-@click.option(
-    '-dd',
-    '--download-dir',
-    type=click.Path(file_okay=False),
-    default=str(Path.cwd()),
-    show_default='current working',
-    callback=_dir_cb,
-    help='Path / URI of the download directory.',
-)
+@split_option
 @click.option(
     '-n/-nn',
     '--nodata/--no-nodata',
@@ -660,8 +637,8 @@ def search(
 @click.option(
     '-dv',
     '--driver',
-    type=click.Choice(Driver, case_sensitive=True),
-    default=Driver.gtiff,
+    type=click.Choice(enums.Driver, case_sensitive=True),
+    default=enums.Driver.gtiff,
     show_default=True,
     help='Format driver for the downloaded file(s).',
 )
@@ -698,6 +675,15 @@ def search(
     help='Maximum number of tiles to decompress concurrently.',
 )
 @click.option(
+    '-dd',
+    '--download-dir',
+    type=click.Path(file_okay=False),
+    default=str(Path.cwd()),
+    show_default='current working',
+    callback=_dir_cb,
+    help='Path / URI of the download directory.',
+)
+@click.option(
     '-o',
     '--overwrite',
     is_flag=True,
@@ -708,13 +694,14 @@ def search(
 def download(
     obj: dict[str, Any],
     image_ids: tuple[str],
-    download_dir: OpenFile,
+    split: enums.SplitType,
     nodata: bool,
-    driver: Driver,
+    driver: enums.Driver,
     max_tile_size: float,
     max_tile_dim: int,
     max_requests: int,
     max_cpus: int,
+    download_dir: OpenFile,
     overwrite: bool,
     **kwargs,
 ):
@@ -731,40 +718,38 @@ def download(
     concurrently.  Tile size can be controlled with --max-tile-size and --max-tile-dim, and
     download / decompress concurrency with --max-requests and --max-cpus.
 
-    Filenames are derived from image Earth Engine IDs.
+    Files are named with the Earth Engine index of their source image, and file band descriptions
+    set to image band names with --split images.  Otherwise, files are named with their band
+    name, and file band descriptions set to the Earth Engine index of the band's source image
+    with --split bands.
     """
     obj['images'] = _get_images(obj, image_ids=image_ids)
-    images = _prepare_images_for_export(
-        obj['images'], cloud_kwargs=obj.get('cloud_kwargs'), **kwargs
-    )
+    coll = _prepare_export_collection(obj['images'], cloud_kwargs=obj.get('cloud_kwargs'), **kwargs)
 
     tqdm.write('\nDownloading:\n')
-    tqdm_kwargs = utils.get_tqdm_kwargs(desc='Total', unit='images')
-    for im in tqdm(images, **tqdm_kwargs):
-        joined_path = posixpath.join(download_dir.path, im.gd.id.replace('/', '-') + '.tif')
-        ofile = OpenFile(download_dir.fs, joined_path, mode='wb')
-        try:
-            im.gd.toGeoTIFF(
-                ofile,
-                overwrite=overwrite,
-                nodata=nodata,
-                driver=driver,
-                max_tile_size=max_tile_size,
-                max_tile_dim=max_tile_dim,
-                max_requests=max_requests,
-                max_cpus=max_cpus,
-            )
-        except FileExistsError as ex:
-            raise click.UsageError(str(ex)) from ex
+    try:
+        coll.gd.toGeoTIFF(
+            download_dir,
+            overwrite=overwrite,
+            split=split,
+            nodata=nodata,
+            driver=driver,
+            max_tile_size=max_tile_size,
+            max_tile_dim=max_tile_dim,
+            max_requests=max_requests,
+            max_cpus=max_cpus,
+        )
+    except FileExistsError as ex:
+        raise click.UsageError(str(ex)) from ex
 
 
 # export command
-@cli.command(epilog='See https://geedim.readthedocs.io/ for more details.')
+@cli.command(cls=_EECommand, epilog='See https://geedim.readthedocs.io/ for more details.')
 @image_ids_option
 @click.option(
     '-t',
     '--type',
-    type=click.Choice(ExportType, case_sensitive=True),
+    type=click.Choice(enums.ExportType, case_sensitive=True),
     default=ImageAccessor._default_export_type.value,
     show_default=True,
     help='Export type.',
@@ -791,6 +776,7 @@ def download(
 @mask_option
 @resampling_option
 @scale_offset_option
+@split_option
 @click.option(
     '-w/-nw',
     '--wait/--no-wait',
@@ -802,8 +788,9 @@ def download(
 def export(
     obj: dict[str, Any],
     image_ids: tuple[str],
-    type: ExportType,
+    type: enums.ExportType,
     folder: str,
+    split: enums.SplitType,
     wait: bool,
     **kwargs,
 ):
@@ -820,39 +807,26 @@ def export(
     concurrently.  Tile size can be controlled with --max-tile-size and --max-tile-dim, and
     download / decompress concurrency with --max-requests and --max-cpus.
 
-    Filenames are derived from image Earth Engine IDs.
+    Files are named with the Earth Engine index of their source image, and file band descriptions
+    set to image band names with --split images.  Otherwise, files are named with their band
+    name, and file band descriptions set to the Earth Engine index of the band's source image
+    with --split bands.
     """
-    if (type in [ExportType.asset, ExportType.cloud]) and not folder:
+    if (type in [enums.ExportType.asset, enums.ExportType.cloud]) and not folder:
         raise click.MissingParameter(param_hint="'-f' / '--folder'", param_type='option')
 
     obj['images'] = _get_images(obj, image_ids=image_ids)
-    images = _prepare_images_for_export(
-        obj['images'], cloud_kwargs=obj.get('cloud_kwargs'), **kwargs
-    )
+    coll = _prepare_export_collection(obj['images'], cloud_kwargs=obj.get('cloud_kwargs'), **kwargs)
 
     tqdm.write('\nExporting:\n')
-    export_tasks = []
-    for im in images:
-        name = im.gd.id.replace('/', '-')
-        try:
-            task = im.gd.toGoogleCloud(name, type=type, folder=folder, wait=False)
-        except ee.EEException as ex:
-            raise click.ClickException(str(ex)) from ex
-        export_tasks.append(task)
-        tqdm.write(f'Started {name}') if not wait else None
-
-    if wait:
-        # wait for export tasks
-        tqdm_kwargs = utils.get_tqdm_kwargs(desc='Total', unit='images')
-        for task in tqdm(export_tasks, **tqdm_kwargs):
-            try:
-                ImageAccessor.monitorTask(task)
-            except (ee.EEException, OSError) as ex:
-                raise click.ClickException(str(ex)) from ex
+    try:
+        coll.gd.toGoogleCloud(type=type, folder=folder, wait=wait, split=split)
+    except (ee.EEException, OSError) as ex:
+        raise click.ClickException(str(ex)) from ex
 
 
 # composite command
-@cli.command(epilog='See https://geedim.readthedocs.io/ for more details.')
+@cli.command(cls=_EECommand, epilog='See https://geedim.readthedocs.io/ for more details.')
 @click.option(
     '-i',
     '--id',
@@ -865,7 +839,7 @@ def export(
     '-cm',
     '--method',
     'method',
-    type=click.Choice(CompositeMethod, case_sensitive=False),
+    type=click.Choice(enums.CompositeMethod, case_sensitive=False),
     default=None,
     show_default="'q-mosaic' for cloud/shadow supported collections, 'mosaic' otherwise.",
     help="Compositing method.",
@@ -881,7 +855,7 @@ def export(
 @click.option(
     '-rs',
     '--resampling',
-    type=click.Choice(ResamplingMethod, case_sensitive=True),
+    type=click.Choice(enums.ResamplingMethod, case_sensitive=True),
     default=ImageAccessor._default_resampling,
     show_default=True,
     help='Resampling method for component images.',
