27baa15dccc03c63820c3b2eaacd5288fbabb403
Dugal Harris <dugalh@gmail.com>
2025-07-27 19:48:25 +0200

    remove unused extensions / sections & add GitHub footer icon

--- docs/conf.py
+++ docs/conf.py
@@ -20,15 +20,13 @@ release = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# TODO: remove unneeded extensions & settings
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosectionlabel',
     'sphinx_click',
     'sphinx_copybutton',
-    # 'jupyter_sphinx',
-    'nbsphinx',
 ]
 
 templates_path = ['_templates']
@@ -37,10 +35,25 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_temp', 'examples']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'furo'
 # html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_title = f'Geedim {__version__}'
+html_theme_options = {
+    # copied from https://github.com/pradyunsg/furo/blob/main/docs/conf.py
+    'footer_icons': [
+        {
+            'name': 'GitHub',
+            'url': 'https://github.com/leftfield-geospatial/geedim',
+            'html': """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            'class': '',
+        }
+    ],
+}
 
 # -- Options for autodoc -----------------------------------------------------
 # see https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
@@ -54,9 +67,7 @@ intersphinx_mapping = {
     'numpy': ('https://numpy.org/doc/stable', None),
     'xarray': ('https://docs.xarray.dev/en/stable', None),
     'rasterio': ('https://rasterio.readthedocs.io/en/stable/', None),
-    'gdal': ('https://gdal.org/', None),
     'fsspec': ('https://filesystem-spec.readthedocs.io/en/latest/', None),
-    'affine': ('https://affine.readthedocs.io/en/latest/', None),
 }
 
 # -- Options for pygments -----------------------------------------------------
@@ -66,18 +77,3 @@ highlight_language = 'none'
 # Make sure the target is unique
 autosectionlabel_prefix_document = True
 # autosectionlabel_maxdepth = 3  # avoid duplicate section labels for CLI examples
-
-# -- Options for nbsphinx --------------------------------------------------
-# env.docname will be e.g. examples/l7_composite.ipynb.  The `../` is to
-# reference it from itself. preferable to link to actual version of the file
-# at the time of the doc build, than a hyperlink to github.
-# see https://github.com/aazuspan/wxee/blob/main/docs/conf.py for other examples
-# TODO: update old geedim nbsphinx options
-nbsphinx_prolog = """
-.. note::
-
-   This page was generated from a Jupyter notebook. To run and interact with it, 
-   you can download it :download:`here <../{{ env.docname }}.ipynb>`.
-"""
-nbsphinx_widgets_path = ''
-nbsphinx_requirejs_path = ''
