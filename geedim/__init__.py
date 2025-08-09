a4d0385b86a00219d212846fa38ef2f4042de8fe
Dugal Harris <dugalh@gmail.com>
2025-08-09 15:11:32 +0200

    import version

--- geedim/__init__.py
+++ geedim/__init__.py
@@ -26,6 +26,7 @@ from geedim.enums import (
 )
 from geedim.mask import MaskedImage
 from geedim.utils import Initialize
+from geedim.version import __version__
 
 # Add a NullHandler to the package logger to hide logs by default.  Applications can
 # then add their own handler(s).
